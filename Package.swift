1d58edbfac222f49cceddef2b096888802e23b9e
Eric Musliner <eric.musliner@gmail.com>
2025-07-02 14:18:37 -0400

    Sets up spm for library with build targets and dependencies
     - Uses swift-crypto instead of CryptoKit for cross-platform support

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IdenticonKit",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "IdenticonKit",
+            targets: ["IdenticonKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0")
+    ],
+    targets: [
+        .target(
+            name: "IdenticonKit",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+            ]
+        ),
+        .testTarget(
+            name: "IdenticonKitTests",
+            dependencies: ["IdenticonKit"]
+        ),
+    ]
+)
