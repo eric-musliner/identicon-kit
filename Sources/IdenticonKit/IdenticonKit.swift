3de363a0fc8dedf41d8fcdc65a0b89413e1b1119
Eric Musliner <eric.musliner@gmail.com>
2025-07-04 13:09:43 -0400

    Makes iconsize enum for specifying preset sizes
     - Confines icon generation with reasonable size limits

--- Sources/IdenticonKit/IdenticonKit.swift
+++ Sources/IdenticonKit/IdenticonKit.swift
@@ -3,7 +3,14 @@ import Foundation
 
 public struct IdenticonKit {
 
-    public init() {}
+    public enum IconSize: Int {
+        case xsmall = 4
+        case small = 5
+        case medium = 8
+        case large = 10
+    }
+
+    private init() {}
 
     /// Generates an identicon SVG based on the given input identifier.
     ///
@@ -12,13 +19,13 @@ public struct IdenticonKit {
     ///   - gridSize: Number of cells in the grid.
     ///   - iconSize: Size of the resulting icon.
     /// - Returns: A valid SVG string representing the identicon.
-    public func generateSvg(
+    static public func generateSvg(
         from identifier: String,
-        size: Int = 5,
+        size: IconSize = .small,
         iconSize: Int = 40
     ) -> String {
 
-        let generator = Generator(gridSize: size, iconSize: iconSize)
+        let generator = Generator(gridSize: size.rawValue, iconSize: iconSize)
         return generator.generate(for: identifier)
     }
 }
